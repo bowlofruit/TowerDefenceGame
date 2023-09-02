b787e192bf03785782441c6ca06f385cb799865c
bowlofruit <ser150448@gmail.com>
2023-09-02 20:50:44 +0300

    Add sells for towers; create path point; create enemy movement

---
+++ Assets/Scripts/PathData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PathData : MonoBehaviour
+{
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform[] _path;
+
+    public Transform StartPoint { get => _startPoint; }
+    public Transform[] PathPoints { get => _path; }
+
+    public static PathData Instantion;
+
+    private void Awake()
+    {
+        Instantion = this;
+    }
+}
